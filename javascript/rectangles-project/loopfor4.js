68cf227a9c529eddefd1c4afd1068ebeb290d63a
evertonprogramadorcriativo <evertonprogramadorcriativo@gmail.com>
2025-06-27 22:27:48 -0300

    exercismo retangulo exemplo loop for 4x

---
+++ javascript/rectangles-project/loopfor4.js
@@ -0,0 +1,13 @@
+for (let i = 0; i < 4; i++) {
+    console.log('andar 1')
+  for (let j = 0; j < 4; j++) {
+    console.log('andar 2')
+    for (let k = 0; k < 4; k++) {
+        console.log('andar 3')
+      for (let l = 0; l < 4; l++) {
+        console.log(`i=${i}, j=${j}, k=${k}, l=${l}`);
+        console.log('andar 4')
+      }
+    }
+  }
+}
